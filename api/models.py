553620c682a47462efe7b842c2d10f7420201e21
Antonio Dias <antonioisaacvd@gmail.com>
2025-03-01 00:25:19 -0300

    :construction: Sobrescreve o método save do model Article para gerar um slug único com base no título do artigo.

--- api/models.py
+++ api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from uuid import uuid4
+from django.utils.text import slugify
 
 class Author(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
@@ -14,7 +15,7 @@ class Author(models.Model):
 class Article(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, blank=True)
     subject = models.CharField(max_length=100)
     resume = models.TextField()
     content = models.TextField()
@@ -22,3 +23,17 @@ class Article(models.Model):
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.title)
+            unique_slug = base_slug
+            counter = 1
+            
+            while Article.objects.filter(slug=unique_slug).exists():
+                unique_slug = f'{base_slug}-{counter}'
+                counter += 1
+                
+            self.slug = unique_slug
+            
+        super().save(*args, **kwargs)
