e5d544e4e55dc943c87fa05b1912eab8a1549123
Antonio Dias <antonioisaacvd@gmail.com>
2025-02-28 23:16:03 -0300

    :wrench: chore: inicializa projeto django e apps 'frontend' e 'api'

---
+++ blog_setuplinuxbr/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for blog_setuplinuxbr project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog_setuplinuxbr.settings')
+
+application = get_asgi_application()
