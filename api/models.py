7fc78e0ff7945b7f9c28c69cdc512ce452bdb79b
Antonio Dias <antonioisaacvd@gmail.com>
2025-02-28 23:27:03 -0300

    :construction: Inicializa os models Author e Article

--- api/models.py
+++ api/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+class Author(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    bio = models.TextField()
+    photo = models.ImageField(upload_to='authors/')
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+class Article(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    subject = models.CharField(max_length=100)
+    resume = models.TextField()
+    image = models.ImageField(upload_to='articles/')
+    content = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
