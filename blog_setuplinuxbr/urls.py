360943b86e92ad821c7a5c835a4950bd9ba71dd7
Antonio Dias <antonioisaacvd@gmail.com>
2025-03-01 05:23:52 -0300

    :construction: Inicializa views do frontend.

--- blog_setuplinuxbr/urls.py
+++ blog_setuplinuxbr/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('', include('frontend.urls')),
 ]
 
 if settings.DEBUG:
