1be348242f31863f00951537052fbc945d739a9a
Antonio Dias <antonioisaacvd@gmail.com>
2025-03-01 01:38:16 -0300

    :wrench: chore: adiciona o .env ao settings.py

--- blog_setuplinuxbr/settings.py
+++ blog_setuplinuxbr/settings.py
@@ -12,19 +12,24 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-041jf4rwv*_p19a1c+$^k4)d@95)blr9w(rixy=%ss7-h6gwe('
+SECRET_KEY = env('SECRET_KEY')
+NYT_API_KEY = env('NYT_API_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG', default=False)
 
 ALLOWED_HOSTS = []
 
