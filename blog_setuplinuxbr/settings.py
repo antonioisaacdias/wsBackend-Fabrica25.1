5c22f623dcb8f652082de7af8de0c6f39c16919d
Antonio Dias <antonioisaacvd@gmail.com>
2025-02-28 23:38:23 -0300

    :wrench: chore: adiciona constantes no settings.py do projeto

--- blog_setuplinuxbr/settings.py
+++ blog_setuplinuxbr/settings.py
@@ -119,10 +119,15 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
